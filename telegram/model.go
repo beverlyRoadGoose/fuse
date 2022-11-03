5bbd31e7a0c4b745e19b293e55fbd2dcfb93ebd2
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2022-11-03 20:54:12 +0100

    conssolidate related types

--- telegram/model.go
+++ telegram/model.go
@@ -1,19 +1,5 @@
 package telegram // import "heytobi.dev/fuse/telegram"
 
-// User represents a Bot user or bot.
-// See https://core.telegram.org/bots/api#user
-type User struct {
-	ID                      int64  `json:"id"`
-	IsBot                   bool   `json:"is_bot"`
-	FirstName               string `json:"first_name"`
-	LastName                string `json:"last_name"`
-	Username                string `json:"username"`
-	LanguageCode            string `json:"language_code"`
-	CanJoinGroups           bool   `json:"can_join_groups"`
-	CanReadAllGroupMessages bool   `json:"can_read_all_group_messages"`
-	SupportsInlineQueries   bool   `json:"supports_inline_queries"`
-}
-
 // BotCommand represents a bot command.
 // See https://core.telegram.org/bots/api#botcommand
 type BotCommand struct {
