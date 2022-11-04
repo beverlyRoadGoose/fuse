eb97dbdb9623503f38f69137c876daf3aea6de9b
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2022-11-04 12:07:42 +0100

    regroup entities (#18)

--- telegram/message.go
+++ telegram/message.go
@@ -72,3 +72,29 @@ type MessageEntity struct {
 	User     *User  `json:"user"`
 	Language string `json:"language"`
 }
+
+// SendMessageRequest defines messages that can be sent by the bot.
+// https://core.telegram.org/bots/api#sendmessage
+type SendMessageRequest struct {
+	ChatID                   int64           `json:"chat_id"`
+	Text                     string          `json:"text"`
+	ParseMode                string          `json:"parse_mode"`
+	Entities                 []MessageEntity `json:"entities"`
+	DisableWebPagePreview    bool            `json:"disable_web_page_preview"`
+	DisableNotification      bool            `json:"disable_notification"`
+	ProtectContent           bool            `json:"protect_content"`
+	ReplyToMessageID         int             `json:"reply_to_message_id"`
+	AllowSendingWithoutReply bool            `json:"allow_sending_without_reply"`
+	// TODO add missing optional reply_markup
+}
+
+type sendMessageResponse struct {
+	Ok     bool `json:"ok"`
+	Result struct {
+		MessageID int    `json:"message_id"`
+		From      *User  `json:"from"`
+		Chat      *Chat  `json:"chat"`
+		Date      int    `json:"date"`
+		Text      string `json:"text"`
+	} `json:"result"`
+}
