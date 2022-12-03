ff3afaad744a88384087bb166b971b2b52451ebb
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2022-12-03 18:34:13 +0100

    correction

--- handlers/conversation_handler.go
+++ handlers/conversation_handler.go
@@ -6,7 +6,8 @@ import (
 
 // ConversationHandler is a suggested default handler. It acts as an orchestrator of the non-command messages received
 // by the bot, by keeping track of the conversation context and delegating actions to the appropriate handler.
-// If it doesn't work will with your use case, you can implement a custom one and register it as your default handler.
+//
+// If it doesn't work well with your use case, you can implement a custom one and register it as your default handler.
 type ConversationHandler struct {
 }
 
